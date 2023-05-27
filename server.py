75713f816701011cae78efc3506cb298d00edcea
Nir Sheep <sheep.nir@gmail.com>
2023-05-27 06:38:34 -0700

    Adding server.py

---
+++ server.py
@@ -0,0 +1,42 @@
+"""
+Creating chatGPT clone that is calling GPT and Dall-E
+"""
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+import openai
+from config import api_key
+
+
+app = Flask(__name__)
+CORS(app)
+
+openai.api_key = api_key
+
+
+def my_chat(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-003", prompt=prompt, max_tokens=100
+    )
+    return response.choices[0].text
+
+
+def my_draw(prompt):
+    response = openai.Image.create(
+        size="256x256", prompt=prompt, n=1, response_format="url"
+    )
+    return response["data"][0]["url"]
+
+
+@app.route("/chatbot")
+def chatbot():
+    pergunta = request.args.get("pergunta")
+    if pergunta[0:4] == "img:":
+        pergunta = pergunta.replace("img:", "")
+        resposta = my_draw(pergunta)
+        return jsonify(url_imagem=resposta, resposta="")
+    else:
+        resposta = my_chat(pergunta)
+        return jsonify(resposta=resposta)
+
+
+app.run()
